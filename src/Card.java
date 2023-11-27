81d7b05e7129ff0943a384aa8415ca06a7c32edd
kdebnath123 <kirin.debnath@menloschool.org>
2023-11-27 09:48:18 -0800

    More example code

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    //all example code
+    private String rank;
+    private int suit;
+
+    public Card(){
+
+        rank = "diamonds";
+        suit = 45;
+    }
+
 }
